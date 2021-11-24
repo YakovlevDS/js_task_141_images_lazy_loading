70ffef96de25f2a51e4b7e9a70f06d32001f63b1
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-11-24 09:28:41 +0200

    added lazy loadding fix

--- app.js
+++ app.js
@@ -16,18 +16,17 @@ window.addEventListener('load', function() {
 });
 
 function lazyLoad() {
-	var card_images = document.querySelectorAll('.card-image');
+	let card_images = document.querySelectorAll('.card-image');
 	
 	// loop over each card image
-	card_images.forEach(function(card_image) {
-		var image_url = card_image.getAttribute('data-image-full');
-		var content_image = card_image.querySelector('img');
-		
+	card_images.forEach(card_image => {
+		let image_url = card_image.getAttribute('data-image-full');
+		let content_image = card_image.querySelector('img');
 		// change the src of the content image to load the new high res photo
 		content_image.src = image_url;
 		
 		// listen for load event when the new photo is finished loading
-		content_image.addEventListener('load', function() {
+		content_image.addEventListener('load', ()=> {
 			// swap out the visible background image with the new fully downloaded photo
 			card_image.style.backgroundImage = 'url(' + image_url + ')';
 			// add a class to remove the blur filter to smoothly transition the image change
@@ -38,15 +37,6 @@ function lazyLoad() {
 	
 }
 
-
-
-//   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-//   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-//   ga('create', 'UA-46156385-1', 'cssscript.com');
-//   ga('send', 'pageview');
 
 
 
